67c2a8104949e52171f077bed65f8c2df101bd46
CheesyPenguin <78714543+CheesyPenguin@users.noreply.github.com>
2022-03-05 23:44:56 +1300

    armor

--- src/main/java/net/astro367/biomeoverhaul/item/ModItems.java
+++ src/main/java/net/astro367/biomeoverhaul/item/ModItems.java
@@ -1,14 +1,13 @@
 package net.astro367.biomeoverhaul.item;
 
 import net.astro367.biomeoverhaul.BiomeOverhaul;
+import net.astro367.biomeoverhaul.item.custom.ModArmorMaterials;
 import net.astro367.biomeoverhaul.item.custom.ModAxeItem;
 import net.astro367.biomeoverhaul.item.custom.ModHoeItem;
 import net.astro367.biomeoverhaul.item.custom.ModPickaxeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.AxeItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ShovelItem;
-import net.minecraft.item.SwordItem;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -24,8 +23,10 @@ public class ModItems {
     public static final Item INCIDIUM_HOE = registerItem("incidium_hoe", new ModHoeItem(ModToolMaterials.INCIDIUM, 0, -3.0f, new FabricItemSettings().group(ModItemGroup.BIOME)));
     public static final Item INCIDIUM_SHOVEL = registerItem("incidium_shovel", new ShovelItem(ModToolMaterials.INCIDIUM, 1.5f, -2.4f, new FabricItemSettings().group(ModItemGroup.BIOME)));
     public static final Item INCIDIUM_PICKAXE = registerItem("incidium_pickaxe", new ModPickaxeItem(ModToolMaterials.INCIDIUM, 2, -2.8f, new FabricItemSettings().group(ModItemGroup.BIOME)));
-
-
+    public static final Item INCIDIUM_HELMET = registerItem("incidium_helmet", new ArmorItem(ModArmorMaterials.INCIDIUM, EquipmentSlot.HEAD, new FabricItemSettings().group(ModItemGroup.BIOME)));
+    public static final Item INCIDIUM_CHESTPLATE  = registerItem("incidium_chestplate", new ArmorItem(ModArmorMaterials.INCIDIUM, EquipmentSlot.CHEST, new FabricItemSettings().group(ModItemGroup.BIOME)));
+    public static final Item INCIDIUM_LEGGINGS = registerItem("incidium_leggings", new ArmorItem(ModArmorMaterials.INCIDIUM, EquipmentSlot.LEGS, new FabricItemSettings().group(ModItemGroup.BIOME)));
+    public static final Item INCIDIUM_BOOTS  = registerItem("incidium_boots", new ArmorItem(ModArmorMaterials.INCIDIUM, EquipmentSlot.FEET , new FabricItemSettings().group(ModItemGroup.BIOME)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(BiomeOverhaul.MOD_ID, name), item);
