754547af4577b17995311bdfad38035506cfbf4b
astro367 <78714325+astro367@users.noreply.github.com>
2022-03-06 10:08:27 +1300

    redwood wood

--- src/main/java/net/astro367/biomeoverhaul/painting/ModPaintings.java
+++ src/main/java/net/astro367/biomeoverhaul/painting/ModPaintings.java
@@ -7,7 +7,7 @@ import net.minecraft.util.registry.Registry;
 
 public class ModPaintings {
 
-    public static final PaintingMotive THEO = registerPainting("theo", new PaintingMotive(32, 32));
+    public static final PaintingMotive THEO = registerPainting("theo", new PaintingMotive(32,32));
 
     private  static PaintingMotive registerPainting(String name, PaintingMotive paintingMotive) {
         return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(BiomeOverhaul.MOD_ID, name), paintingMotive );
