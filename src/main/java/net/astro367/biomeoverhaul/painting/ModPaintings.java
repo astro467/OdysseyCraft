c4edced2d7248cb625b0571cd549d5dd4181677e
CheesyPenguin <78714543+CheesyPenguin@users.noreply.github.com>
2022-03-06 16:19:42 +1300

    shrek painting

--- src/main/java/net/astro367/biomeoverhaul/painting/ModPaintings.java
+++ src/main/java/net/astro367/biomeoverhaul/painting/ModPaintings.java
@@ -9,6 +9,7 @@ public class ModPaintings {
 
     public static final PaintingMotive THEO = registerPainting("theo", new PaintingMotive(32,32));
     public static final PaintingMotive DAISYCAT = registerPainting("daisycat", new PaintingMotive(64,64));
+    public static final PaintingMotive SHREK = registerPainting("shrek", new PaintingMotive(48, 48));
 
     private  static PaintingMotive registerPainting(String name, PaintingMotive paintingMotive) {
         return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(BiomeOverhaul.MOD_ID, name), paintingMotive );
