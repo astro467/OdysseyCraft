dad23c1f3594eac2a65f1d0e1762cd55f7a45211
astro367 <78714325+astro367@users.noreply.github.com>
2022-03-06 13:25:24 +1300

    redwood buttons, pressureplates, fences and fence gates

--- src/main/java/net/astro367/biomeoverhaul/block/ModBlocks.java
+++ src/main/java/net/astro367/biomeoverhaul/block/ModBlocks.java
@@ -1,13 +1,12 @@
 package net.astro367.biomeoverhaul.block;
 
 import net.astro367.biomeoverhaul.BiomeOverhaul;
+import net.astro367.biomeoverhaul.block.custom.ModPressurePlateBlock;
+import net.astro367.biomeoverhaul.block.custom.ModWoodenButtonBlock;
 import net.astro367.biomeoverhaul.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
-import net.minecraft.block.PillarBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -24,9 +23,10 @@ public class ModBlocks {
     public static final Block REDWOOD_WOOD = registerBlock("redwood_wood", new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD)), ModItemGroup.BIOME);
     public static final Block STRIPPED_REDWOOD_WOOD = registerBlock("stripped_redwood_wood", new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD)), ModItemGroup.BIOME);
     public static final Block REDWOOD_PLANKS = registerBlock("redwood_planks", new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS)), ModItemGroup.BIOME);
-
-
-
+    public static final Block REDWOOD_BUTTON = registerBlock("redwood_button", new ModWoodenButtonBlock(FabricBlockSettings.copy(Blocks.OAK_BUTTON)), ModItemGroup.BIOME);
+    public static final Block REDWOOD_PRESSURE_PLATE = registerBlock("redwood_pressure_plate", new ModPressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings.copy(Blocks.OAK_PRESSURE_PLATE)), ModItemGroup.BIOME);
+    public static final Block REDWOOD_FENCE = registerBlock("redwood_fence", new FenceBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE)), ModItemGroup.BIOME);
+    public static final Block REDWOOD_FENCE_GATE = registerBlock("redwood_fence_gate", new FenceGateBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE_GATE)), ModItemGroup.BIOME);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {registerBlockItem(name, block, group);return Registry.register(Registry.BLOCK, new Identifier(BiomeOverhaul.MOD_ID, name), block);}
     private static Item registerBlockItem(String name, Block block, ItemGroup group) {return Registry.register(Registry.ITEM, new Identifier(BiomeOverhaul.MOD_ID, name),new BlockItem(block, new FabricItemSettings().group(group)));}
---
+++ src/main/java/net/astro367/biomeoverhaul/block/custom/ModWoodenButtonBlock.java
@@ -0,0 +1,9 @@
+package net.astro367.biomeoverhaul.block.custom;
+
+import net.minecraft.block.WoodenButtonBlock;
+
+public class ModWoodenButtonBlock extends WoodenButtonBlock {
+    public ModWoodenButtonBlock(Settings settings) {
+        super(settings);
+    }
+}
